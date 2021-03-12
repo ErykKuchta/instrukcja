e3972fd765835a259f44f9305dc78abae5e6742d
unknown <erkuchta@gmail.com>
2021-03-12 09:59:11 +0100

    system 15-pietrowego budynku

---
+++ system.java
@@ -0,0 +1,27 @@
+class instrukcje {
+
+	public static void main(String args[]){
+
+int [] pietro = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15};
+System.out.println( pietro[0] + " " + "nazwa firmy");
+System.out.println( pietro[1] + " " + "nazwa firmy");
+System.out.println( pietro[2] + " " + "nazwa firmy");
+System.out.println( pietro[3] + " " + "nazwa firmy");
+System.out.println( pietro[4] + " " + "nazwa firmy");
+System.out.println( pietro[5] + " " + "nazwa firmy");
+System.out.println( pietro[6] + " " + "nazwa firmy");
+System.out.println( pietro[7] + " " + "nazwa firmy");
+System.out.println( pietro[8] + " " + "nazwa firmy");
+System.out.println( pietro[9] + " " + "nazwa firmy");
+System.out.println( pietro[10] + " " + "nazwa firmy");
+System.out.println( pietro[11] + " " + "nazwa firmy");
+System.out.println( pietro[12] + " " + "nazwa firmy");
+System.out.println( pietro[13] + " " + "nazwa firmy");
+System.out.println( pietro[14] + " " + "nazwa firmy");
+
+
+
+		
+		
+	}
+}
